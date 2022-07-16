8b2fa4c6711f668f3edfa861dc13496b1c221e80
Aldijana Zahirović <aldijanazahirovic406@gmail.com>
2022-07-15 21:15:38 -0700

    Add project files.

---
+++ MDI/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace MDI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.datotekaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pomoćToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.otvoriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.štampajToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zatvoriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.indexToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oAplikacijiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.datotekaToolStripMenuItem,
+            this.pomoćToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(782, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // datotekaToolStripMenuItem
+            // 
+            this.datotekaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.otvoriToolStripMenuItem,
+            this.štampajToolStripMenuItem,
+            this.zatvoriToolStripMenuItem});
+            this.datotekaToolStripMenuItem.Name = "datotekaToolStripMenuItem";
+            this.datotekaToolStripMenuItem.Size = new System.Drawing.Size(84, 24);
+            this.datotekaToolStripMenuItem.Text = "Datoteka";
+            // 
+            // pomoćToolStripMenuItem
+            // 
+            this.pomoćToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.indexToolStripMenuItem,
+            this.oAplikacijiToolStripMenuItem});
+            this.pomoćToolStripMenuItem.Name = "pomoćToolStripMenuItem";
+            this.pomoćToolStripMenuItem.Size = new System.Drawing.Size(68, 24);
+            this.pomoćToolStripMenuItem.Text = "Pomoć";
+            // 
+            // otvoriToolStripMenuItem
+            // 
+            this.otvoriToolStripMenuItem.Name = "otvoriToolStripMenuItem";
+            this.otvoriToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.otvoriToolStripMenuItem.Text = "Otvori";
+            this.otvoriToolStripMenuItem.Click += new System.EventHandler(this.Otvori);
+            // 
+            // štampajToolStripMenuItem
+            // 
+            this.štampajToolStripMenuItem.Name = "štampajToolStripMenuItem";
+            this.štampajToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.štampajToolStripMenuItem.Text = "Štampaj";
+            // 
+            // zatvoriToolStripMenuItem
+            // 
+            this.zatvoriToolStripMenuItem.Name = "zatvoriToolStripMenuItem";
+            this.zatvoriToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.zatvoriToolStripMenuItem.Text = "Zatvori";
+            this.zatvoriToolStripMenuItem.Click += new System.EventHandler(this.Zatvori);
+            // 
+            // indexToolStripMenuItem
+            // 
+            this.indexToolStripMenuItem.Name = "indexToolStripMenuItem";
+            this.indexToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.indexToolStripMenuItem.Text = "Index";
+            // 
+            // oAplikacijiToolStripMenuItem
+            // 
+            this.oAplikacijiToolStripMenuItem.Name = "oAplikacijiToolStripMenuItem";
+            this.oAplikacijiToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.oAplikacijiToolStripMenuItem.Text = "O aplikaciji";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "MDI testna aplikacija";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem datotekaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem otvoriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem štampajToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zatvoriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pomoćToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem indexToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oAplikacijiToolStripMenuItem;
+    }
+}
+
---
+++ MDI/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MDI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Otvori(object sender, EventArgs e)
+        {
+            Form oChildForm = new Form2();
+            oChildForm.MdiParent = this;
+            oChildForm.Show();
+        }
+
+        private void Zatvori(object sender, EventArgs e)
+        {
+            bool bZatvoreno = false;
+            foreach (Form childForm in MdiChildren)
+            { 
+                if ((childForm == ActiveMdiChild) && !bZatvoreno)
+                {
+                    childForm.Close();
+                    bZatvoreno = true;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
